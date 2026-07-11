9a82bd7a775abc6a76de8c59aab4ae5202dc0fd1
rhayakyrhaykend <d03791933@gmail.com>
2026-07-11 20:52:41 -0300

    Adicionando service worker

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'entregapro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/calculadora.html',
+  '/gerador.html',
+  '/manifest.json',
+  '/icon-72.png',
+  '/icon-96.png',
+  '/icon-128.png',
+  '/icon-144.png',
+  '/icon-192.png',
+  '/icon-384.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
